92ee39ce12bf60bcb7847369133587670c8670ed
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-01 22:32:00 -0400

    completed

--- lib/shoe.py
+++ lib/shoe.py
@@ -6,14 +6,39 @@ class Shoe:
         self.brand = brand
         self.size = size
 
-    def get_size(self):
-        pass
-
-    def set_size(self, size):
-        pass
     
-    size = property(get_size, set_size)
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            print(f"Setting size to { size }")
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    
+    # def get_size(self):
+    #     print("Retrieving size.")
+    #     return self._size
+
+    # def set_size(self, size):
+    #     if isinstance(size, int):
+    #         print(f"Setting size to { size }")
+    #         self._size = size
+    #     else:
+    #         print("size must be an integer")
+    
+    # size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
 
 
-# shoe = Shoe("Nike", 7)
+shoe = Shoe("Nike", 7)
+shoe.size = 8
+shoe.size = "hello"
     
