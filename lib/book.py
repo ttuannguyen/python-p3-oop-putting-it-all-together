d674c073f1bd8a7f9b848b1071548d2afacc5da0
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-01 21:54:43 -0400

    wip

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
     
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    
+    def get_page_count(self):
+        print("Retrieving page count.")
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            print(f"Setting page count to { page_count }")
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+book = Book("Awesome", 100)
+# book.turn_page()
         
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def get_size(self):
+        pass
+
+    def set_size(self, size):
+        pass
+    
+    size = property(get_size, set_size)
+
+
+# shoe = Shoe("Nike", 7)
+    
